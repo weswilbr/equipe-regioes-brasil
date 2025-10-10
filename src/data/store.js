536c5c40ab3e434b92c80d73089877e16637eb00
weswilbr <165349896+weswilbr@users.noreply.github.com>
2025-10-10 10:58:02 -0300

    Atualizar o store.js

--- src/data/store.js
+++ src/data/store.js
@@ -1,4 +1,4 @@
-// Localização na Estrutura: /src/data/store.js (CORRIGIDO)
+// Localização na Estrutura: /src/data/store.js
 
 import { LOCAL_STORAGE_KEY } from '../config/config.js';
 import { getTranslatedString } from '../config/i18n.js';
